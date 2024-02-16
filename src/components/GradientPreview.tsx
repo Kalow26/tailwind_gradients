21e686cd0c9ac4798d188b5bf8a88e52cfdefefe
Laurent De La Calle <laurentdelacalle@hotmail.com>
2024-02-16 18:42:14 +0100

    feat : add a button to copy to ClipBoard

--- src/components/GradientPreview.tsx
+++ src/components/GradientPreview.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { useRef, useState } from "react";
 import { useStore } from "../store/store";
+import { Button } from "./ui/button";
 
 const GradientPreview = () => {
+  const [copied, setCopied] = useState(false);
+  const tailwindClassNameRef = useRef(null);
   const orientation = useStore((state) => state.orientation);
   const startColor = useStore((state) => state.startColor);
   const endColor = useStore((state) => state.endColor);
@@ -20,6 +24,25 @@ const GradientPreview = () => {
     3
   )} from-${tailwindStartColorName}-${tailwindStartRefColor} to-${tailwindEndColorName}-${tailwindEndRefColor}`;
 
+  const copyToClipboard = () => {
+    if (tailwindClassNameRef.current) {
+      const textToCopy =
+        (tailwindClassNameRef.current as HTMLElement)?.textContent ?? "";
+
+      navigator.clipboard
+        .writeText(textToCopy)
+        .then(() => {
+          setCopied(true);
+          setTimeout(() => setCopied(false), 1000);
+        })
+        .catch((error) => {
+          console.error(
+            "Erreur lors de la copie dans le presse-papiers : ",
+            error
+          );
+        });
+    }
+  };
   return (
     <>
       <div
@@ -29,9 +52,15 @@ const GradientPreview = () => {
         }}
       ></div>
       <span className="text-center text-sm">Tailwind Class</span>
-      <p className="text-center p-2 border border-slate-900 font-bold">
+      <p
+        className="text-center p-2 border border-slate-900 font-bold"
+        ref={tailwindClassNameRef}
+      >
         {tailwindClassName}
       </p>
+      <Button onClick={copyToClipboard}>
+        {copied ? "Copied!" : "Copy to Clipboard"}
+      </Button>
     </>
   );
 };
