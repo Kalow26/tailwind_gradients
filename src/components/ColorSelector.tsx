a81dd34d6105bfca5c48542ddbb5cedc4b35b4be
Laurent De La Calle <laurentdelacalle@hotmail.com>
2024-02-16 18:12:51 +0100

    refactor : changed the options design and improved ux/ui feat : add the Metadata

--- src/components/ColorSelector.tsx
+++ src/components/ColorSelector.tsx
@@ -5,7 +5,7 @@ import GradientPreview from "./GradientPreview";
 const ColorSelector = () => {
   return (
     <section className="xl:container mt-10">
-      <div className="w-full h-[700px] border border-slate-300 p-4 rounded-2xl shadow-md">
+      <div className="w-full h-max-content border border-slate-300 p-4 rounded-2xl shadow-md">
         <div className="flex h-full justify-between flex-1">
           <GradientGrid />
           <div className="flex flex-col p-6 flex-1 gap-6">
--- src/components/GradientOptions.tsx
+++ src/components/GradientOptions.tsx
@@ -1,29 +1,53 @@
 "use client";
 import { Label } from "@/components/ui/label";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { useStore } from "../store/store";
+import { Button } from "./ui/button";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
 
 const GradientOptions = () => {
-  const orientation = useStore((state) => state.orientation);
   const setOrientation = useStore((state) => state.setOrientation);
   const setGradientPosition = useStore((state) => state.setGradientPosition);
+  const tailwindStartref = useStore((state) => state.tailwindStartRefColor);
+  const tailwindEndref = useStore((state) => state.tailwindEndRefColor);
+  const tailwindStartColor = useStore((state) => state.tailwindStartColorName);
+  const tailwindEndColor = useStore((state) => state.tailwindEndColorName);
+  const startColor = useStore((state) => state.startColor);
+  const endColor = useStore((state) => state.endColor);
+  const orientation = useStore((state) => state.orientation);
+
   return (
-    <form className="flex flex-col justify-between border border-slate-300 p-4 rounded-2xl">
-      <Label htmlFor="orientation" className="pb-4">
-        Choose your gradient orientation
-      </Label>
-      <select
-        name="orientation"
-        id="orientation"
-        onChange={(e) => setOrientation(e.currentTarget.value)}
-        value={orientation} // Set the default value here
-        className="w-full p-2 border border-slate-900 rounded-md"
-      >
-        <option value="to right">Right</option>
-        <option value="to left">Left</option>
-        <option value="to top">Top</option>
-        <option value="to bottom">Bottom</option>
-      </select>
+    <div className="flex flex-col justify-between border border-slate-300 p-4 rounded-2xl">
+      <Label className="text-sm pb-2">Gradient orientation</Label>
+      <div className="flex gap-2">
+        <Button
+          value="to right"
+          onClick={(e) => setOrientation(e.currentTarget.value)}
+          className={`${orientation === "to right" ? "bg-blue-500" : ""}`}
+        >
+          Right
+        </Button>
+        <Button
+          value="to left"
+          onClick={(e) => setOrientation(e.currentTarget.value)}
+          className={`${orientation === "to left" ? "bg-blue-500" : ""}`}
+        >
+          Left
+        </Button>
+        <Button
+          value="to bottom"
+          onClick={(e) => setOrientation(e.currentTarget.value)}
+          className={`${orientation === "to bottom" ? "bg-blue-500" : ""}`}
+        >
+          Bottom
+        </Button>
+        <Button
+          value="to top"
+          onClick={(e) => setOrientation(e.currentTarget.value)}
+          className={`${orientation === "to top" ? "bg-blue-500" : ""}`}
+        >
+          Top
+        </Button>
+      </div>
 
       <RadioGroup defaultValue="from" className="pt-6">
         <Label htmlFor="orientation" className="pb-2">
@@ -31,22 +55,48 @@ const GradientOptions = () => {
         </Label>
         <div className="flex items-center space-x-2">
           <RadioGroupItem
+            className="hidden"
             value="from"
             id="from"
             onClick={(e) => setGradientPosition(e.currentTarget.value)}
-          />
-          <Label htmlFor="from">from</Label>
+          ></RadioGroupItem>
+
+          <Label
+            htmlFor="from"
+            className="cursor-pointer w-full h-16 relative rounded-md"
+            style={{ backgroundColor: startColor }}
+          >
+            <span className="p-2 bg-white text-black absolute top-1 left-1 text-xs rounded-md">
+              Start Color
+            </span>
+            <span className="p-2 bg-white text-black absolute bottom-1 right-1 text-xs rounded-md">
+              {tailwindStartColor}-{tailwindStartref}
+            </span>
+          </Label>
         </div>
         <div className="flex items-center space-x-2">
           <RadioGroupItem
             value="to"
             id="to"
             onClick={(e) => setGradientPosition(e.currentTarget.value)}
-          />
-          <Label htmlFor="to">to</Label>
+            className="hidden"
+          ></RadioGroupItem>
+
+          <Label
+            htmlFor="to"
+            className="cursor-pointer w-full h-16 relative rounded-md"
+            style={{ backgroundColor: endColor }}
+          >
+            <span className="p-2 bg-white text-black absolute top-1 left-1 text-xs rounded-md">
+              End Color
+            </span>
+            <span className="p-2 bg-white text-black absolute bottom-1 right-1 text-xs rounded-md">
+              {tailwindEndColor}-{tailwindEndref}
+            </span>
+          </Label>
         </div>
       </RadioGroup>
-    </form>
+    </div>
   );
 };
 
